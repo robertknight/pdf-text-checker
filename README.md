9b48e29be5e871d9d0060e64c18b91a23d8ad60f
Robert Knight <robertknight@gmail.com>
2020-10-03 11:42:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# PDF text checker
+
+This is a small web application that retrieves a PDF from a URL and
+checks whether it has selectable text that can be extracted by
+[PDF.js](https://mozilla.github.io/pdf.js/)
+
+A PDF that has selectable text is suitable for use with tools like [Hypothesis](https://hypothes.is).
+PDFs without selectable text will require pre-processing with an
+[OCR](https://en.wikipedia.org/wiki/Optical_character_recognition) tool.
+
+## Usage
+
+Start a local HTTP server with:
+
+```
+python3 -m http.server 5002
+```
+
+Then browse to http://localhost:5002/check-pdf-text.html. Enter the URL
+of a PDF to fetch and check and click "Check PDF".
+
+## Notes
+
+This tool relies on a proxy server to bypass cross-origin restrictions on fetching
+URLs. It currently uses https://cors-anywhere.herokuapp.com for this purpose.
+
+Checking whether a PDF's text layer is usable for the purposes of copying and
+annotating text is not a binary classification. A PDF may have selectable text
+that is garbled or otherwise problematic. This tool uses very basic heuristics
+and the result does not guarantee that the PDF will have "usable" text.
---
+++ check-pdf-text.js
@@ -0,0 +1,97 @@
+'use strict';
+
+function range(min, max) {
+  const result = [];
+  for (let i = min; i < max; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+/**
+ * Return a URL that proxies `url` to add CORS headers to allow cross-origin
+ * access.
+ */
+function corsProxyUrl(url) {
+  return `https://cors-anywhere.herokuapp.com/${url}`;
+}
+
+/**
+ * Load the PDF.js library.
+ *
+ * Returns the entry point to the loaded library.
+ * See https://github.com/mozilla/pdf.js/blob/master/src/display/api.js for API.
+ */
+async function loadPDFJS() {
+  if (window.pdfjsLib) {
+    return window.pdfjsLib;
+  }
+
+  const script = document.createElement('script');
+  script.src = 'https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.min.js';
+
+  return new Promise((resolve, reject) => {
+    script.addEventListener('load', () => resolve(window.pdfjsLib));
+    script.addEventListener('error', e => reject(e.error));
+    document.body.appendChild(script);
+  });
+}
+
+function showStatus(text) {
+  const statusEl = document.getElementById('status');
+  statusEl.textContent = text;
+}
+
+/**
+ * Check whether the PDF at `url` has text that PDF.js can extract.
+ */
+async function checkPDFTextLayer(url) {
+  showStatus('Loading PDF library...');
+  const pdfjs = await loadPDFJS();
+
+  showStatus(`Loading PDF from ${url}...`);
+  const loadingTask = pdfjs.getDocument(corsProxyUrl(url));
+  let doc;
+
+  try {
+    doc = await loadingTask.promise;
+  } catch (err) {
+    showStatus(`Failed to load PDF: ${err}`);
+    return;
+  }
+
+  const getPageText = async index => {
+    const page = await doc.getPage(index + 1);
+    const textContent = await page.getTextContent();
+    return textContent.items.map(it => it.str).join(' ');
+  };
+
+  // Check the first N pages. The number of pages checked is a balance between
+  // skipping any "banner" pages in front of the content that might not contain
+  // text and optimizing the speed of the check.
+  const maxPages = Math.min(5, doc.numPages);
+  showStatus(`Checking text of first ${maxPages} pages...`);
+  let pageTexts;
+
+  try {
+    pageTexts = await Promise.all(range(0, maxPages).map(i => getPageText(i)));
+  } catch (err) {
+    showStatus(`Unable to fetch text from pages: ${err}`);
+    return;
+  }
+
+  // TODO - Add smarter text quality metrics here.
+  const hasText = pageTexts.some(pt => pt.length > 10);
+
+  if (hasText) {
+    showStatus('PDF has extractable text');
+  } else {
+    showStatus('PDF does not have extractable text. You will need to OCR it.');
+  }
+}
+
+const checkButton = document.getElementById('checkPdfButton');
+checkButton.onclick = () => {
+  const url = document.getElementById('pdfUrlInput').value;
+  checkPDFTextLayer(url);
+};
